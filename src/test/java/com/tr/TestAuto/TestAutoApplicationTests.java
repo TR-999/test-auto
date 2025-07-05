bebcf5a2bae2e3b21eb4e7ab563f6007ff38fad4
Tarmizie Razali <tarmizierazali96@gmail.com>
2025-07-05 12:11:49 +0800

    initial

---
+++ src/test/java/com/tr/TestAuto/TestAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tr.TestAuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
